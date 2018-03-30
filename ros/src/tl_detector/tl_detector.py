9a9f97aa1a51adb6f380c57c5ca33a0a106c4a89
Andreas Menge <lunar9@gmx.de>
2018-03-30 09:55:00 -0700

    Well behaving traffic light detection.
    Tested in simulator without latest changes in master: Reasonable accuracy.
    TODO: test with latest master to check whether it can be merged together.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -13,12 +13,18 @@ import yaml
 
 import math
 
-STATE_COUNT_THRESHOLD = 2
+import os
+import rospkg
+image_path = rospkg.RosPack().get_path('tl_detector') + '/light_classification/Traffic_light/aaa_new_samples/'
 
+
+STATE_COUNT_THRESHOLD = 2
 
 class TLDetector(object):
     def __init__(self):
         rospy.init_node('tl_detector')
+
+        print(image_path)
 
         self.pose = None
         self.waypoints = None
@@ -28,7 +34,7 @@ class TLDetector(object):
 
         self.listener = None
 
-        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
         self.waypoints = rospy.wait_for_message('/base_waypoints', Lane).waypoints  # Only need to get base_waypoints once
         '''
         /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
@@ -37,7 +43,7 @@ class TLDetector(object):
         simulator. When testing on the vehicle, the color state will not be available. You'll need to
         rely on the position of the light and the camera image to predict it.
         '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb, queue_size=1)
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb, queue_size=1)
         # Only need to get first message from /vehicle/traffic_lights once traffic light classifier is implemented
         # self.lights = rospy.wait_for_message('/vehicle/traffic_lights', TrafficLightArray).lights
 
@@ -50,7 +56,7 @@ class TLDetector(object):
             tl.pose.pose.position.x, tl.pose.pose.position.y, tl.pose.pose.position.z = slp[0], slp[1], 0
             self.slps.append(self.get_closest_waypoint(tl.pose.pose.position))
 
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        rospy.Subscriber('/image_color', Image, self.image_cb)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
@@ -196,7 +202,22 @@ class TLDetector(object):
             # TODO use light location to zoom in on traffic light in image
 
             # Get classification
-            return self.light_classifier.get_classification(cv_image)
+
+            pred = self.light_classifier.get_classification(cv_image)
+
+            # # # Here we automatically store wrongly classified images into a sub-folder for later re-training:
+            # if pred != light.state:
+            #     folder = {TrafficLight.RED: 'red/',
+            #               TrafficLight.YELLOW: 'yellow/',
+            #               TrafficLight.GREEN: 'green/'}
+            #     filepath = image_path + folder[light.state]
+            #     path, dirs, files = os.walk(filepath).next()
+            #     filename = filepath + 'wrong_' + str(len(files)).zfill(5) + '.png'
+            #     RGB_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            #     cv2.imwrite(filename, RGB_img)
+            #     rospy.logwarn("Light state: %s,   Light detected: %s", light.state, pred)
+
+            return pred
 
 
     def process_traffic_lights(self):
@@ -227,7 +248,7 @@ class TLDetector(object):
             fake_state = light.state  # Remove once traffic light classifier is implemented
             state = self.get_light_state(light)
 
-            rospy.logwarn("Light wp: %s,  Light state: %s,   Light detected: %s", light_wp, fake_state, state)
+            # rospy.logwarn("Light wp: %s,  Light state: %s,   Light detected: %s", light_wp, fake_state, state)
 
             return light_wp, state
 
